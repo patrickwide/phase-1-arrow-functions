929f80ea5d0010a736759a7c5eed150c509ab382
patrickwide <patrickwide254@gmail.com>
2023-03-15 03:43:45 +0300

    complete assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+// function expression
+const divide = function () {
+  return 2000 / 100;
+};
+
+// arrow function
+const square = (num) => num * num;
+
+// arrow function with implicit return
+const add = (num1, num2) => num1 + num2;
